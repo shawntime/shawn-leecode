0d51c35520679c9bbb748d30d289f0783f15ced0
mashaohua <mashaohua@autohome.com.cn>
2021-08-13 11:17:48 +0800

    环形链表

---
+++ leecode/src/main/java/com/shawntime/leecode/链表/_141_环形链表.java
@@ -0,0 +1,34 @@
+package com.shawntime.leecode.链表;
+
+/**
+ * https://leetcode-cn.com/problems/linked-list-cycle/
+ */
+public class _141_环形链表 {
+
+    /**
+     * 快慢指针
+     */
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode first = head;
+        ListNode second = head.next;
+        while (second != null && second.next != null) {
+            first = first.next;
+            second = second.next.next;
+            if (first == second) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
